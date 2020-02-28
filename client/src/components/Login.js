525ec031adf5e90f8fe6a0dc10ecf72fe9a8b3e2
carlsachs <sachscarl@gmail.com>
2020-02-28 11:31:59 -0500

    created login page and added empty axios with auth inside utils folder

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [ login, setLogin ] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setLogin({
+        ...login,
+        [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+        .post('login', login)
+        .then(res => {
+            console.log(res);
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/creatures');
+        })
+        .catch(err => console.log(`Login.js axiosWithAuth error:`, err.response ))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
